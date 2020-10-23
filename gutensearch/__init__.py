da2a27cc6204c878a4983720f9b00fd753542c55
Yannis Katsaros <yannis@katsaros.io>
2020-10-22 21:48:14 -0500

    Add function to download text files from zip file and fix mypy issues

--- gutensearch/__init__.py
+++ gutensearch/__init__.py
@@ -1,1 +1,4 @@
-from .download import get_site_urls
+from .download import (
+    get_site_urls,
+    download_zip_text,
+)
--- gutensearch/download.py
+++ gutensearch/download.py
@@ -4,10 +4,13 @@ Downloads and saves a local copy of every English document
 and respectful way. Alternatively, the download can be
 parameterized to limit the total number of documents downloaded.
 """
-from typing import List
+import os
+from typing import List, Dict, Union, Sequence
+from io import BytesIO
+from zipfile import ZipFile
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # type: ignore
 
 def get_site_urls(url: str) -> List[str]:
     """
@@ -27,10 +30,44 @@ def get_site_urls(url: str) -> List[str]:
     response = requests.get(url)
     response.raise_for_status()
 
-    soup = BeautifulSoup(response.text)
+    soup = BeautifulSoup(response.text, features='html.parser')
     links = [t.attrs.get('href') for t in soup.find_all('a')]
     
     return [l for l in links if l is not None]
 
-def download_zip(url: str) -> str:
-    ...
+def download_zip_text(url: str) -> List[Dict[str, Union[str, Sequence[str]]]]:
+    """
+    Download and parse the contents of all text files
+    from the given URL string of a zip file, stripping
+    any leading/trailing whitespace from each line of each file.
+
+    Parameters:
+        url: The URL string of the zip file
+
+    Returns:
+        A list of dictionaries, where each dictionary contains
+        the id of the document, and the lines parsed as utf-8 strings.
+
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # open the zip file
+    zfile = ZipFile(BytesIO(response.content))
+    # and only keep any docs with .txt attachments
+    txt_files = [f for f in zfile.namelist() if f.endswith('.txt')]
+
+    contents = []
+    for file in txt_files:
+        # first get the file name (needed for the document id)
+        name, _ = os.path.splitext(file)
+
+        # then open and parse the file contents as UTF-8 strings
+        with zfile.open(file) as f:
+            lines = [l.decode('utf-8').strip() for l in f.readlines()]
+            contents.append({
+                'id': name,
+                'lines': lines,
+            })
+
+    return contents
