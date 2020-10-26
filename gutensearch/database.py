46b9dac0ad526212e113d40fffff794645ca255c
Yannis Katsaros <yannis@katsaros.io>
2020-10-25 22:07:43 -0500

    Add generic utility function to insert records into the db

--- gutensearch/database.py
+++ gutensearch/database.py
@@ -4,9 +4,10 @@ Postgres database, including inserting/loading data and
 executing a variety of queries/searches.
 """
 import os
-from typing import Dict
+from typing import Dict, Any, Iterable
 
 import psycopg2
+from psycopg2.extensions import connection # type: ignore
 
 POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
@@ -34,3 +35,34 @@ def dbconfig() -> Dict[str, str]:
         'user': POSTGRES_USER,
         'password': POSTGRES_PASSWORD,
     }
+
+def insert_records(con: connection, table: str, records: Iterable[Dict[str, Any]]):
+    """
+    Generic utility function to insert records in the form of
+    a sequence of dictionaries, where each key in the dictionary
+    represents the column name in the specified table.
+
+    Parameters:
+        con: Connection to the Postgres database
+        table: The name of the table to insert the records into
+        records:
+            A sequence (iterable) of dictionaries, where each
+            dictionary is inserted as a single "row" in the table
+    """
+    # do nothing for empty records
+    if len(records) == 0:
+        return
+
+    params = [f'%({c})s' for c in records[0].keys()]
+    wildcards = f'({", ".join(params)})'
+
+    # generate the SQL statement - doesn't actually bind any
+    # external data apart from the table name provided by the caller
+    sql = f"""
+    INSERT INTO {table}
+    VALUES {wildcards}
+    """
+    with con.cursor() as cur:
+        cur.executemany(sql, records)
+
+    con.commit()
