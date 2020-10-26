3f15390945d99f7cc08371322c07f215ebd62803
Yannis Katsaros <yannis@katsaros.io>
2020-10-26 09:23:16 -0500

    Don't create indexes upon init

--- schema.sql
+++ schema.sql
@@ -3,6 +3,3 @@ CREATE TABLE IF NOT EXISTS words (
     document_id BIGINT NOT NULL,
     count INT NOT NULL
 );
-
-CREATE INDEX idx_words_word ON words (word);
-CREATE INDEX idx_words_id ON words(document_id);
